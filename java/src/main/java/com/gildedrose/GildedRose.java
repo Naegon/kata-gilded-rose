9b77e70ef6c29109cbe9f4d6b8adcdb3c3cb235f
Naegon <hamelgui@gmail.com>
2022-01-11 15:45:27 +0100

    Moved increment and decrement quality functions outside of main loop

--- java/src/main/java/com/gildedrose/GildedRose.java
+++ java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,9 @@
 package com.gildedrose;
 
 import static com.gildedrose.Utils.*;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
 class GildedRose {
     Item[] items;
 
@@ -14,24 +17,9 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (item.name.equals(Utils.AGED_BRIE) || item.name.equals(Utils.BACKSTAGE_PASSES)) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals(Utils.BACKSTAGE_PASSES)) {
-                        if (item.sellIn < 11 && item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-
-                        if (item.sellIn < 6 && item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
-            } else {
-                if (item.quality > 0 && !item.name.equals(Utils.SULFURAS)) {
-                    item.quality = item.quality - 1;
-                }
+            switch (item.name) {
+                case AGED_BRIE, BACKSTAGE_PASSES -> item.quality = incrementQuality(item);
+                default -> item.quality = decrementQuality(item);
             }
 
             if (!item.name.equals(SULFURAS)) {
@@ -54,5 +42,22 @@ class GildedRose {
                 }
             }
         }
+    }
+
+    static int decrementQuality(Item item) {
+        return max(0, item.name.equals(SULFURAS)? item.quality : item.quality -1);
+    }
+
+    static int incrementQuality(Item item) {
+        int quality = item.quality;
+
+        quality += 1;
+
+        if (item.name.equals(BACKSTAGE_PASSES)) {
+            if (item.sellIn < 11) quality += 1;
+            if (item.sellIn < 6) quality += 1;
+        }
+
+        return min(quality, 50);
     }
 }
--- java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,6 +6,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.gildedrose.GildedRose.*;
 import static com.gildedrose.Utils.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,6 +22,102 @@ class GildedRoseTest {
 
         assertEquals(19, foo.sellIn);
         assertEquals(9, foo.quality);
+    }
+
+    @Test
+    public void decrementQuality_Test() {
+        // Given
+        Item mongooseElixir = new Item(MONGOOSE_ELIXIR, 5, 25);
+
+        // When
+        mongooseElixir.quality = decrementQuality(mongooseElixir);
+
+        // Then
+        assertEquals(24, mongooseElixir.quality);
+    }
+
+    @Test
+    public void decrementQuality_TestWithSulfuras() {
+        // Given
+        Item sulfuras = new Item(SULFURAS, 5, 25);
+
+        // When
+        sulfuras.quality = decrementQuality(sulfuras);
+
+        // Then
+        assertEquals(25, sulfuras.quality);
+    }
+
+    @Test
+    public void decrementQuality_TestWithNegativeQuality() {
+        // Given
+        Item mongooseElixir = new Item(MONGOOSE_ELIXIR, 5, 0);
+
+        // When
+        mongooseElixir.quality = decrementQuality(mongooseElixir);
+
+        // Then
+        assertEquals(0, mongooseElixir.quality);
+    }
+
+    @Test
+    public void incrementQuality_Test() {
+        // Given
+        Item agedBrie = new Item(AGED_BRIE, 5, 25);
+
+        // When
+        agedBrie.quality = incrementQuality(agedBrie);
+
+        // Then
+        assertEquals(26, agedBrie.quality);
+    }
+
+    @Test
+    public void incrementQuality_TestWithMaxedQuality() {
+        // Given
+        Item agedBrie = new Item(AGED_BRIE, 5, 50);
+
+        // When
+        agedBrie.quality = incrementQuality(agedBrie);
+
+        // Then
+        assertEquals(50, agedBrie.quality);
+    }
+
+    @Test
+    public void incrementQuality_TestWithDistantBackstagePasses() {
+        // Given
+        Item backstagePasses = new Item(BACKSTAGE_PASSES, 12, 25);
+
+        // When
+        backstagePasses.quality = incrementQuality(backstagePasses);
+
+        // Then
+        assertEquals(26, backstagePasses.quality);
+    }
+
+    @Test
+    public void incrementQuality_TestWithMediumBackstagePasses() {
+        // Given
+        Item backstagePasses = new Item(BACKSTAGE_PASSES, 8, 25);
+
+        // When
+        backstagePasses.quality = incrementQuality(backstagePasses);
+
+        // Then
+        assertEquals(27, backstagePasses.quality);
+    }
+
+    @Test
+    public void incrementQuality_TestWithImminentBackstagePasses() {
+        // Given
+        Item backstagePasses = new Item(BACKSTAGE_PASSES, 2, 25);
+
+        // When
+        backstagePasses.quality = incrementQuality(backstagePasses);
+
+        // Then
+        assertEquals(28, backstagePasses.quality);
     }
 
     @Test
